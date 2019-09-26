05d9b6ebe18e328beca412bc8c6aafa6e379f7c9
snordale <snordale@gmail.com>
2019-09-26 10:39:25 -0400

    implemented fit function

--- hello.py
+++ hello.py
@@ -36,34 +36,75 @@ with open ('winequality-red.csv', 'r') as f:
     
 
 class LinearRegression:
+	import sys 
+	import numpy as np 
+	import matplotlib.pyplot as mpl 
+	import csv 
+	import pandas as pd 
+
+	def __init__(self,x,w):  
+		self.x = x;
+		self.w = w;
 
 	def sigmoid(w, x):
 		a = np.dot(w, x)
 		return 1/(1 + np.exp(-a))
 
-	def loss(s, y):
+	def loss(w, x, y):
 		arr = []
-		for x in y:
-			arr.append(-y * np.log(s) - (1 - y) * np.log(1 - h))
-		return np.mean(arr)
+		w = 0
+		s = sigmoid(w, x)
+		for i in len(y):
+			w += arr.append(-y[i] * np.log(s) + (1 - y[i]) * np.log(1 - s))
+		return w/len(y)
 
-	def graddes(w, x, y):
+	def descent(w, x, y):
 		sum = 0
 		for i in range(y.size):
 			sum += np.dot(x[i], (y[i] - np.dot(w, x[i])))
 		return sum
 	
 	def update(w, a, g):
-		return w - (a * g)
+		return np.subtract(w(a * g))
 
+	def fit(w, x, y, i, a, sigmoid, loss, descent, update):
+		new = x
+		for j in i:
+			sig = sigmoid(w, new)
+			loss = loss(s, y)
+			grade = descent(w, new, y)
+			new = update(new, a, grade)
+		return sigmoid(new, x)
 
+	with open ('winequality-red.csv', 'r') as f: 
+		wines = list(csv.reader(f, delimiter=';'))
+		wines = np.array(wines[1:], dtype = np.float)
 
+	quality = np.array(wines[:,11], dtype = np.float) 
 
+	df = pd.DataFrame(quality, columns = ['quality'])
+
+	df.loc[df.quality <=5, 'binary classification'] = 0 
+	df.loc[df.quality  >5, 'binary classification'] = 1
 
 	
+	numpy_matrix = df.as_matrix()
+	
+	binaryclassification = (numpy_matrix[:,1])
+	
+	dataset = np.insert(wines, 12, binaryclassification, axis = 1)
 
-
-   
-   
-    
+	quality = []
+	for x in dataset:
+		quality.append(x[12])
+	mpl.hist(quality)
+	mpl.show()
+			
+	wines = np.array(wines[1:], dtype = np.float)
+	data = pd.read_csv("winequality-red.csv")
+	quality = np.array(wines[:,11], dtype = np.float)
+	df = pd.DataFrame(quality, columns = ['quality'])
+	print(df)
+	df.loc[df.quality <=5, 'binary classification'] = 0 
+	df.loc[df.quality  >5, 'binary classification'] = 1
 
