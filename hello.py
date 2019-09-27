77c6eee0c81febe52ddd3817a6ecf34e2b0cdcf7
Hafsa Khalid <hafsakhalid@Hafsas-MacBook-Air.local>
2019-09-27 12:32:49 -0400

    progress

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as mpl 
 import csv 
 import pandas as pd 
-
+from numpy import array 
 
 with open ('winequality-red.csv', 'r') as f: 
 	wines = list(csv.reader(f, delimiter=';'))
@@ -15,6 +15,8 @@ with open ('winequality-red.csv', 'r') as f:
 	numpy_matrix = df.as_matrix()
 	#print(numpy_matrix)
 	binaryclassification = (numpy_matrix[:,1])
+	y = np.array([binaryclassification])
+	
 	#print(binaryclassification)
 	#print(binaryclassification.shape)
 	#dataset = np.insert(wines, 12, binaryclassification, axis = 1)
@@ -28,51 +30,56 @@ with open ('winequality-red.csv', 'r') as f:
 	#print(y.shape)
 
 
-class LinearRegression: 
+#y = binaryclassification
+print(y)
+print(y.shape)
+X = wines
+print(X)
+w = np.zeros((12, 1), dtype=np.float)
+print(w)
 
-	y = binaryclassification
-	print(y)
-	#print(y.shape)
-	X = wines 
-	print(X)
-	w = np.zeros((12, 1), dtype=np.int)
-	print(w)
+def sigmoid (s):
+	return 1/1+ (np.exp(-s))
 
+def gradient_descent (X, Y, w):
+	sum = 0
+	r = Y.size
+	for i in range(r):
+		#expected gives the value [2,]
+		y = np.array([Y[i]])
+		print(Y.shape)
+		print(y.shape)
+		print(y[i])
+		x =  np.array([X[i]])
+		print(x.shape)
+		print(w.shape)
+		expected = sigmoid(np.dot(x, w))
+		print(expected.shape)
+		#expec = np.array([expected])
+		print(expected)
+		#print(expec.shape)
+		d = Y[i] - expected
+		#Y[i] gives you an array of 1's and 0's 
+		delta = np.array([d])
+		print(delta.shape)
+		x =  np.array([X[i]])
+		print(x.shape)
+		for j in X[i]:
+			sum += X[i] * delta.T
+	return sum
 
-	def fit (X, y, w): 
-		a = np.dot(w, x)
-		sig = 1/(1 + np.exp(-a))
-		gradient = np.dot(X.T, (sig - y)) / x.shape[1]
-		w = w - .1 * gradient
-		return 
+def update (w, a, X, Y):
 
+	for i in range(Y.size):
+		w[i] = w[i] + a * gradient_descent(X, Y, w)
+	return w
 
+def fit (X, Y, w, iter, a):
+	for i in range(iter):
+		w = update(w, a, X, Y)
+	result = sigmoid (np.dot(w.T,X))
 
-    
-    
-		
-	
-	#def sigmoid(w, x):
-		#a = np.dot(w, x)
-		#return 1/(1 + np.exp(-a))
-
-	def loss(s, y):
-		arr = []
-		for x in y:
-			arr.append(-y * np.log(s) - (1 - y) * np.log(1 - h))
-		return np.mean(arr)
-
-	def graddes(w, x, y):
-		sum = 0
-		for i in range(y.size):
-			sum += np.dot(x[i], (y[i] - np.dot(w, x[i])))
-		return sum
-	
-	def update(w, a, g):
-		return w - (a * g)
-
-
-
+print(fit(wines, y, w, 200, 0.1))
 
 
 
