c8e3241198bb4ad5f7442f9ac9a2dd756d78dcbc
snordale <snordale@gmail.com>
2019-09-27 15:46:03 -0400

    ok

--- hello.py
+++ hello.py
@@ -3,7 +3,47 @@ import numpy as np
 import matplotlib.pyplot as mpl 
 import csv 
 import pandas as pd 
-from numpy import array 
+from numpy import array
+
+class LogisticRegression:
+
+	def __init__(self, rate, iterations):
+		self.rate = rate
+		self.iterations = iterations
+		
+
+	def sigmoid (self, x):
+		return 1/(1 + (np.exp(-x)))
+
+	def loss(self, sigmoid, y):
+		return (-y * np.log(sigmoid) + (1 - y) * np.log(1 - sigmoid)).mean()
+
+	def gradient_descent (self, X, Y, sigmoid):
+		return (np.dot(X.T, (Y - sigmoid).T))
+
+	def update (self, weights, rate, gradient):
+		return weights - rate * gradient
+
+	def fit (self, X, Y):
+		self.weights = np.zeros(X.shape[1])
+
+
+		for i in range(self.iterations):
+			s = self.sigmoid(np.dot(self.weights, X.T))
+			gradient = self.gradient_descent(X, Y, s)
+			self.weights = self.update(self.weights, self.rate, gradient)
+		return self.weights
+	
+
+	def predict(self, X):
+		outputs = np.zeros(X.shape[1])
+		for i in range(outputs.size):
+			print(self.weights)
+			if self.sigmoid(np.dot(self.weights, X.T)) > 0.5:
+				outputs[i] = 1
+			else:
+				outputs[i] = 0
+		return outputs
 
 with open ('winequality-red.csv', 'r') as f: 
 	wines = list(csv.reader(f, delimiter=';'))
@@ -16,75 +56,20 @@ with open ('winequality-red.csv', 'r') as f:
 	#print(numpy_matrix)
 	binaryclassification = (numpy_matrix[:,1])
 	y = np.array([binaryclassification])
-	
-	#print(binaryclassification)
-	#print(binaryclassification.shape)
-	#dataset = np.insert(wines, 12, binaryclassification, axis = 1)
-	#print(dataset)
-	#this is the array of the 1's and 0's 
-	#y = []
-	#for x in dataset:
-		#y.append(x[12])
-	#print(y)
-	#print(wines)
-	#print(y.shape)
-
-
-#y = binaryclassification
-print(y)
-print(y.shape)
-X = wines
-print(X)
-w = np.zeros((12, 1), dtype=np.float)
-print(w)
-
-def sigmoid (s):
-	return 1/1+ (np.exp(-s))
-
-def gradient_descent (X, Y, w):
-	sum = 0
-	r = Y.size
-	for i in range(r):
-		#expected gives the value [2,]
-		y = np.array([Y[i]])
-		print(Y.shape)
-		print(y.shape)
-		print(y[i])
-		x =  np.array([X[i]])
-		print(x.shape)
-		print(w.shape)
-		expected = sigmoid(np.dot(x, w))
-		print(expected.shape)
-		#expec = np.array([expected])
-		print(expected)
-		#print(expec.shape)
-		d = Y[i] - expected
-		#Y[i] gives you an array of 1's and 0's 
-		delta = np.array([d])
-		print(delta.shape)
-		x =  np.array([X[i]])
-		print(x.shape)
-		for j in X[i]:
-			sum += X[i] * delta.T
-	return sum
-
-def update (w, a, X, Y):
-
-	for i in range(Y.size):
-		w[i] = w[i] + a * gradient_descent(X, Y, w)
-	return w
-
-def fit (X, Y, w, iter, a):
-	for i in range(iter):
-		w = update(w, a, X, Y)
-	result = sigmoid (np.dot(w.T,X))
-
-print(fit(wines, y, w, 200, 0.1))
-
-
-
 
 	
+	
+	X = wines
+	
+	weights = np.zeros((12, 1), dtype=np.float)
+	
+
+	rate = 0.01
+	iterations = 100
+
+	model = LogisticRegression(rate, iterations)
+	model.fit(wines, y)
+	results = model.predict(wines)
 
 
    
