a6c5f258cd4847175a706be29476a04e00579df4
Hafsa Khalid <hafsakhalid@Hafsas-MacBook-Air.local>
2019-10-03 17:07:26 -0400

    changes

--- execute.py
+++ execute.py
@@ -5,6 +5,7 @@ import pandas as pd
 from numpy import array
 import math
 import random
+import time
 from log import *
 from lda import *
 
@@ -35,14 +36,8 @@ with open ('winequality-red.csv', 'r') as f:
 			# if f == 0:
 			# 	matrix = np.column_stack(([1] * len(feature_arrays[0]), matrix))
 
-	
 
 	binaryclassification = np.copy((matrix[:,-1]))
-	
-
-
-
-
 
 	for i in range(len(binaryclassification)):
 		if binaryclassification[i] > 5.0:
@@ -51,28 +46,139 @@ with open ('winequality-red.csv', 'r') as f:
 			binaryclassification[i] = 0
 
 	y = binaryclassification
+
 	X = matrix
 	k = 5
-	def validation(X, k):
+
+	def split(X, k):
 		folds = []
-		data = X
-		fold_size = int(len(data)/k)
-
 		for i in range(k):
-			fold = []
-			for i in range(fold_size):
-				return folds
+			folds.append([])
+		data = np.copy(X)
 
-	folds = validation(X, k)
+		it = 0
+		while len(data) != 0:
+			f = it % k
+	
+			index = random.randint(0, len(data) - 1)
 
-	rate = 0.002
-	iterations = 200
+			folds[f].append(data[index])
+
+			data = np.delete(data, index, 0)
+
+			it += 1
+		return folds
+
+	rate = 0.0001
+	iterations = 10
 	weights = [1] * len(matrix[0])
 
-	model = LogisticRegression(rate, iterations)
-	new_weights = model.fit(X, y, weights)
+	k = 3
+	folds = split(X, k)
 
-	results = model.predict(matrix, new_weights)
-	print(model.evaluate_acc(y, results))
-lda_model = LDA()
-logodds = lda_model.fit(X, y)
+	log_results = np.zeros(12)
+	log_results = np.reshape(log_results, (12, 1))
+
+	lda_results = np.zeros(5)
+
+	for i in range(k):
+		t_set = np.zeros((0, 12))
+		v_set = np.zeros((0, 12))
+		v_set = np.append(v_set, folds[i], 0)
+		
+		for j in range(k):
+			if j != i:
+				t_set = np.append(t_set, folds[j], 0)
+
+		t_set_outputs = np.copy((t_set[:,-1]))
+
+		for i in range(len(t_set_outputs)):
+			if t_set_outputs[i] > 5.0:
+				t_set_outputs[i] = 1
+			else:
+				t_set_outputs[i] = 0
+
+		v_set_outputs = np.copy((v_set[:,-1]))
+		for i in range(len(v_set_outputs)):
+			if v_set_outputs[i] > 5.0:
+				v_set_outputs[i] = 1
+			else:
+				v_set_outputs[i] = 0
+
+		log_model = LogisticRegression(rate, iterations)
+
+		new_weights = log_model.fit(t_set, t_set_outputs, weights)
+		new_weights = np.reshape(new_weights, (12, 1))
+		
+
+		log_results = np.append(log_results, new_weights, axis=1)
+		
+		
+		lda_model = LDA()
+		
+		lda_values = lda_model.fit(t_set, t_set_outputs)
+
+		lda_results = np.append(lda_results, lda_values)
+		
+	
+	log_results = np.delete(log_results, 0, axis=1)
+	print(log_results)
+	log_results_avg = log_results.mean(axis = 1)
+	
+	lda_results_avg = lda_results.mean()
+
+	
+
+
+	print(v_set)
+	print(v_set_outputs)
+	print(log_results_avg)
+	log_projections = log_model.predict(v_set, log_results_avg)
+	
+	
+	log_accuracy = log_model.evaluate_acc(v_set_outputs, log_projections)
+	
+	log_start = time.time()
+	log_model = LogisticRegression(rate, iterations)
+	new_weights = log_model.fit(t_set, t_set_outputs, weights)
+	log_projections = log_model.predict(X, new_weights)
+	log_accuracy = log_model.evaluate_acc(t_set_outputs, log_projections)
+	log_end = time.time()
+	log_time = log_end - log_start 
+
+	print("Logistic Regression Accuracy: {}".format(log_accuracy))
+	print("Execution Time: {}".format(log_time))
+
+
+
+	lda_model = LDA()
+	lda_values = lda_model.fit(t_set, t_set_outputs)
+	lda_projections = lda_model.predict(X, lda_values)
+	lda_accuracy = lda_model.evaluate_acc(lda_projections, t_set_outputs)
+
+	
+	print("LDA Accuracy: {lda_accuracy}")
+		
+
+
+
+# 	model = LogisticRegression(rate, iterations)
+# 	new_weights = model.fit(X, y, weights)
+
+# 	results = model.predict(matrix, new_weights)
+# 	print(model.evaluate_acc(y, results))
+	
+# lda_model = LDA()
+# values = lda_model.fit(X, y)
+# projections = lda_model.predict(X, values)
+# print(lda_model.evaluate_acc(projections, y))
+# k_folds = 3
+
+
+
+
+
+
+
+
+
--- lda.py
+++ lda.py
@@ -4,34 +4,86 @@ import matplotlib.pyplot as mpl
 import csv 
 import pandas as pd 
 from numpy import array
+from numpy import newaxis
 import math
 import random
 
 
 class LDA:
 
-    def fit(self, X, y):
-        class0 = []
-        class1 = []
+	def fit(self, X, y):
+		class0 = []
+		class1 = []
+
+		for i in range(len(y)):
+			if y[i] == 1:
+				class1.append(y[i])
+			else:
+				class0.append(y[i])
+
+		probability0 = len(class0)/len(y)
+		probability1 = len(class1)/len(y)
+
+		data1 = np.copy(X[(X[:, -1] > 5)])
+		data0 = np.copy(X[(X[:, -1] <= 5)])
+
+		mew0 = np.mean(data0, axis = 0)
+		mew1 = np.mean(data1, axis = 0)
+
+		mew00 = mew0[:, newaxis]
+		mew11 = mew1[:, newaxis]
+		
 
 
-        for i in range(len(y)):
-            if y[i] == 1:
-                class1.append(y[i])
-            else:
-                class0.append(y[i])
 
-        probability0 = len(class0)/len(y)
-        probability1 = len(class1)/len(y)
+		avg_v0 = np.zeros((12, 12))
+		avg_v1 = np.zeros((12, 12))
 
-        data1 = np.copy(X[(X[:, -1] > 5)])
-        data0 = np.copy(X[(X[:, -1] <= 5)])
-        print(data0)
-        print()
-        print(data1)
+		for i in range(len(data0)):
+			
+			avg_v0 += np.dot((data0[i] - mew00), (data0[i] - mew00).transpose())
 
-        mew0 = np.mean(data0, axis = 0)
-        mew1 = np.mean(data1, axis = 0)
+		for i in range(len(data1)):
+			
+			avg_v1 += np.dot((data1[i] - mew11), (data1[i] - mew11).transpose())
+			
 
-        print(mew0)
-        print(mew1)
+		avg_sum = avg_v1 + avg_v0
+
+		covariance = avg_sum / (len(X) - 2)
+		
+		
+		values = np.array([probability0, probability1, mew00, mew11, covariance])
+		return values
+
+	def predict(self, X, values):
+		prob0 = values[0]
+		prob1 = values[1]
+		avg0 = values[2]
+		avg1 = values[3]
+		inverse = np.linalg.inv(values[4])
+
+		threshhold = -0.1
+		projections = []
+
+		for i in range(len(X)):	
+			d_bound = np.log(prob1/prob0) - (1/2) * np.dot(np.dot(avg1.transpose(), inverse), avg1) + (1/2) * np.dot(np.dot(avg0.transpose(), inverse), avg0) + np.dot(np.dot(X[i], inverse), (avg1 - avg0))
+			if d_bound > threshhold:
+				projections.append(1)
+			else:
+				projections.append(0)
+
+		return projections
+
+
+	def evaluate_acc(self, projections, real):
+		correct = 0
+		for i in range(len(real)):
+			if projections[i] == real[i]:
+				correct += 1
+		return (correct / len(real))
+
+
+
+
+
